5ccf210e8cf3660e0d3cc050b74a9e7ac6147edd
baoanh1310 <dataholicguy@gmail.com>
2022-03-15 07:57:18 +0700

    load vietocr utils

--- utils.py
+++ utils.py
@@ -13,11 +13,12 @@ def load_vietocr_model():
     from PIL import Image
     from vietocr.tool.config import Cfg
     from vietocr.tool.predictor import Predictor
+    from config import VIETOCR_CHECKPOINT
 
     # Load VietOCR model
     print("Loading VietOCR model...")
     config = Cfg.load_config_from_name('vgg_transformer')
-    config['weights'] = 'https://drive.google.com/uc?id=13327Y1tz1ohsm5YZMyXVMPIOjoOA0OaA'
+    config['weights'] = VIETOCR_CHECKPOINT
     config['cnn']['pretrained'] = False
     config['device'] = device
     config['predictor']['beamsearch'] = False
