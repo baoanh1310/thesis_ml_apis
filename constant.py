270ac66d5cad412851fb1e1ac2f3d2d5d67a2ade
baoanh1310 <dataholicguy@gmail.com>
2022-03-30 12:34:22 +0700

    remove unnecessary label_dict key-value for unknown class

--- constant.py
+++ constant.py
@@ -1,2 +1,2 @@
-label_dict = {0: 'ecg', 1: 'oxygenmeter', 2: 'prescription', 3: 'scales', 4: 'sphygmomanometer', 5: 'thermometer', 100: 'unknown'}
+label_dict = {0: 'ecg', 1: 'oxygenmeter', 2: 'prescription', 3: 'scales', 4: 'sphygmomanometer', 5: 'thermometer'}
 CLASSIFIER_THRESHOLD = 3.01983642578125
