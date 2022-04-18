829a9e3d882f0f242427a67b47c9994797f05eea
baoanh1310 <dataholicguy@gmail.com>
2022-04-18 11:08:46 +0700

    Change threshold

--- constant.py
+++ constant.py
@@ -1,2 +1,2 @@
 label_dict = {0: 'ecg', 1: 'oxygenmeter', 2: 'prescription', 3: 'scales', 4: 'sphygmomanometer', 5: 'thermometer'}
-CLASSIFIER_THRESHOLD = 3.01983642578125
+CLASSIFIER_THRESHOLD = 2.8898143768310547
